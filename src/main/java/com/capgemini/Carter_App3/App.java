e2c967d7ca5386d2ab343911c561f8ed11933a4b
bjakkula <bjakkula@LIN80016387.corp.capgemini.com>
2019-03-05 15:19:13 +0530

    added new project

---
+++ src/main/java/com/capgemini/Carter_App3/App.java
@@ -0,0 +1,10 @@
+package com.capgemini.Carter_App3;
+
+/** * Hello world! * */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
